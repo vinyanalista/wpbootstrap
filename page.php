2a1434f0621e8d733db338916603b70e52d213dc
Antônio Vinícius <vinyanalista@gmail.com>
2015-08-16 00:26:05 -0300

    Template das páginas

---
+++ page.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<div class="row">
+    <div class="col-md-8">
+
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+            <h1><?php the_title(); ?></h1>
+            <div class="entry-content">
+                <?php the_content(); ?>
+            </div>
+
+        <?php endwhile; else: ?>
+            <p><?php _e('Desculpe, essa página não existe.'); ?></p>
+        <?php endif; ?>
+
+    </div>
+    <div class="col-md-4">
+        <!-- Aqui virá a barra lateral -->
+    </div>
+</div>
+
+<?php get_footer(); ?>
